6fc2da0ed533bc4378f9ab6bef2561936f8678ef
Pratibha <pratibha.yadav97@gmail.com>
2024-02-20 12:08:36 +0530

    JS

--- Frontend/simple-calculator/app.js
+++ Frontend/simple-calculator/app.js
@@ -1,25 +1,24 @@
-let inputEl = document.querySelector('input')
-let btns = document.querySelectorAll('button')
-
+let inputEl=document.querySelector("input")
+let btns=document.querySelectorAll("button")
 
 for(let button of btns){
-    button.addEventListener('click' , function(e){
-        let btnText = e.target.innerText;
-        // console.log(btnText)
-        if(btnText === 'C'){
-            inputEl.value = "";
-        } 
-        else if(btnText === '='){
-            try{ //sahi kaam
-                inputEl.value = eval(inputEl.value);
+    button.addEventListener("click",function(e){
+        let btnText=e.target.innerText;
+        console.log(btnText);
+
+        if(btnText==='C'){
+            inputEl.value=""
+        }
+        else if(btnText==="="){
+            try{ //if correct
+                inputEl.value=eval(inputEl.value)
             }
-            catch(err){ //error handling
-                inputEl.value = "invalid"
+            catch{ //error handling
+                inputEl.value="invalid"
             }
-            
         }
         else{
-            inputEl.value = inputEl.value + btnText; 
-        }
+            inputEl.value=inputEl.value+btnText;
+        }       
     })
 }
